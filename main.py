58269dbafc7476c4708b50d3f42f45f87aac13bc
parzi-val <krbala1511@gmail.com>
2022-11-24 22:30:16 +0530

    added font-sizes

--- main.py
+++ main.py
@@ -17,14 +17,20 @@ from tkinter import (
     ttk,
     Label,
     BOTH,
+    IntVar,
 )
 
 root = Tk()
 root.geometry("700x370")
 root.title("Text Editor")
 root.configure(bg="light gray")
+
 font_str = StringVar()
+font_size = IntVar()
+
 string = "Calibri"
+size = 12
+
 menu_bar = Menu(root)
 
 fileMenu = Menu(menu_bar, tearoff=0)
@@ -40,13 +46,14 @@ def FindMenu():
         sub = entry.get()
         val = typeWindow.get("1.0", "end-1c")
 
-        if sub not in val:
+        if sub not in val or not val or not sub:
             messagebox.showerror("Error", "No matching occurences found!")
 
         index = 1
         c = 0
         res = []
         text = val.split("\n")
+
         for string in text:
             i = 0
             j = len(sub)
@@ -71,16 +78,28 @@ def FindMenu():
         typeWindow.delete("1.0", "end-1c")
         typeWindow.insert("1.0", val)
         typeWindow.tag_configure("highlight", background="yellow", foreground="black")
+
         for pos in res:
             typeWindow.tag_add("highlight", pos[0], pos[1])
+
         popup.destroy()
 
     popup = Toplevel(root)
+    popup.transient(root)
     popup.title("Find")
     popup.geometry("300x100")
+
     entry = Entry(popup, width=25)
     entry.place(x=30, y=35)
-    button = Button(popup, text="Find", command=find, padx=10)
+
+    button = Button(
+        popup,
+        text="Find",
+        font=("Courier", 11, "bold"),
+        command=find,
+        padx=10,
+        relief=GROOVE,
+    )
     button.place(x=200, y=32)
 
 
@@ -98,6 +117,7 @@ def ReplaceMenu():
         res = []
         text = val.split("\n")
         val = ""
+
         for string in text:
             i = 0
             j = len(sub)
@@ -124,46 +144,66 @@ def ReplaceMenu():
                     j = i + len(sub)
             val += "\n"
             index += 1
+
         typeWindow.delete("1.0", "end-1c")
         typeWindow.insert("1.0", val)
         typeWindow.tag_configure("highlight", background="yellow", foreground="black")
+
         for pos in res:
             typeWindow.tag_add("highlight", pos[0], pos[1])
+
         popup.destroy()
 
     popup = Toplevel(root)
+    popup.transient(root)
     popup.title("Replace")
     popup.geometry("300x100")
-    label1 = Label(popup, text="Find : ")
-    label1.place(x=30, y=35)
+
+    label1 = Label(popup, text="Find", font=("Courier", 13, "bold"))
+    label1.place(x=40, y=20)
+
     entry1 = Entry(popup, width=25)
-    entry1.place(x=112, y=35)
+    entry1.place(x=112, y=23)
+
     entry2 = Entry(popup, width=25)
-    entry2.place(x=30, y=70)
-    button = Button(popup, text="Replace", command=replace, padx=10)
-    button.place(x=200, y=67)
+    entry2.place(x=30, y=55)
+
+    button = Button(popup, text="Replace", command=replace, padx=10, relief=GROOVE)
+    button.place(x=200, y=52)
 
 
 def FontStyle():
     global font_str
     fonts = font.families()
+    sizes = [i for i in range(8, 30)]
     font_str.set(string)
+    font_size.set(size)
 
     def font_set():
-        global font_str, string
+        global font_str, string, size
         fontstyle = fontlist.get()
+        size_font = fontsize.get()
         string = fontstyle
-        typeWindow.config(font=(f"{fontstyle}",))
+        size = size_font
+        typeWindow.config(font=(f"{fontstyle}", size_font))
         popup.destroy()
 
     popup = Toplevel(root)
+    popup.transient(root)
     popup.title("Font Style")
     popup.geometry("300x100")
+
     fontlist = ttk.Combobox(popup, textvariable=font_str, state="readonly")
     fontlist["values"] = fonts
     fontlist.current(fonts.index(string))
-    fontlist.place(x=30, y=35)
-    button = Button(popup, text="Apply", command=font_set, padx=10)
+    fontlist.place(x=30, y=25)
+
+    fontsize = ttk.Combobox(popup, textvariable=font_size, state="readonly")
+    fontsize["values"] = sizes
+    fontsize.current(sizes.index(int(size)))
+    fontsize.place(x=30, y=45)
+
+    button = Button(popup, text="Apply", command=font_set, padx=10, relief=GROOVE)
     button.place(x=200, y=32)
 
 
@@ -171,7 +211,6 @@ fileMenu.add_command(label="Find", command=FindMenu)
 fileMenu.add_command(label="Replace", command=ReplaceMenu)
 editMenu.add_command(label="Copy")
 editMenu.add_command(label="Cut")
-fontMenu.add_command(label="Font Size")
 fontMenu.add_command(label="Font Style", command=FontStyle)
 fontMenu.add_command(label="Format")
 
